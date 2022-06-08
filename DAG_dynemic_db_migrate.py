d851de46cd968337516ff8260f6dda244d7d299f
Egor_Neliubin <neo.8318731@gmail.com>
2022-06-08 17:40:10 +0300

    dynemic tree - unto table level

--- DAG_dynemic_db_migrate.py
+++ DAG_dynemic_db_migrate.py
@@ -5,6 +5,7 @@ import time as tm
 import datetime as dt
 import xml.etree.ElementTree as ET
 
+
 args = {
     'owner': 'airflow',
     'start_date': dt.datetime(2020, 2, 11),
@@ -13,35 +14,96 @@ args = {
     'depends_on_past': False,
 }
 
-def step(**kwargs) :
-    key1 = kwargs['key1']
-    print(f'LOL {key1}')
 
-xml_file = '/home/airflow/airflow/dags/P2_AIRFLOW_DYNEMIC_DAG/config.xml'
+path = '/home/airflow/airflow/dags/P2_AIRFLOW_DYNEMIC_DAG/'
+log_file = f'{path}logfile.log'
+xml_file = f'{path}config.xml'
 xml_root = ET.parse(xml_file).getroot()
-task_id = 0
+task_npp = 0
+task_name = None
+task_mess = None
+xml_obj_name = None
+xml_obj = None 
+xml_obj_pr = None
+
+
+def print_log(task_mess, task_id) :
+    c_dt = dt.datetime.now()
+    if task_id == 1 :
+        with open(log_file, "w") as o : 
+            input_str = "\n" + '-' * 100 + '\n' + str(c_dt) + ': ' + task_mess
+            o.write(input_str)
+    else :
+        with open(log_file, "a") as o : 
+            input_str = "\n" + str(c_dt) + ': Start ' + task_mess
+            o.write(input_str)
+
+def step(task_mess,task_id) :
+    print_log(task_mess, task_id)
+
+def cr_py_operator(task_npp, task_name, task_mess) :
+    r_operator = PythonOperator(
+            task_id=task_name,
+            python_callable = step,
+            op_kwargs={'task_mess': task_mess , 'task_id' : task_npp},
+            dag=dag
+            )
+    return r_operator
+
 
 with DAG(dag_id = 'DAG_dynemic_tree', default_args = args, schedule_interval = None) as dag:
 
     tasks = {}
+    trees = []
 
-    for sourse in xml_root :
-        cur_sourse = sourse.attrib['name']
-        task_id += 1
-        tasks[cur_sourse] = PythonOperator(
-            task_id = f'T{task_id}_{cur_sourse}',
-            python_callable = step,
-            op_kwargs={'key1': 'LOLec'},
-            dag=dag
-            )
-        #for db in sourse :
-        #    cur_db = db.attrib["name"].lower()
-        #    tasks[cur_db] = PythonOperator(
-        #        task_id='step1',
-        #        python_callable=step1,
-        #        op_kwargs={'key1': 'LOL'},
-        #        dag=dag
-        #    )
+    xml_obj_pr = xml_root
+    for xml_obj in xml_obj_pr :
+        xml_obj_name = xml_obj.attrib['name']
+        task_npp += 1
+        task_name = f'T{task_npp}_{xml_obj_name}'
+        task_mess = f'T{task_npp}: {xml_obj_name}'
+        tasks[task_name] = cr_py_operator(task_npp, task_name, task_mess)
+        task_name_for_next_step = task_name
+        xml_obj_pr = xml_obj
+        for xml_obj in xml_obj_pr :
+            xml_obj_name = xml_obj.attrib["name"].lower()
+            task_npp += 1
+            task_name = f'T{task_npp}_{xml_obj_name}'
+            task_mess = f'T{task_npp}: {xml_obj_name}'
+            tasks[task_name] = cr_py_operator(task_npp, task_name, task_mess)
+            trees.append(f'{task_name_for_next_step}>{task_name}')
+            task_name_for_next_step = task_name
+            xml_obj_pr = xml_obj
+            for xml_obj in xml_obj_pr :
+                xml_obj_name = xml_obj.attrib["name"].lower()
+                task_npp += 1
+                task_name = f'T{task_npp}_{xml_obj_name}'
+                task_mess = f'T{task_npp}: {xml_obj_name}'
+                tasks[task_name] = cr_py_operator(task_npp, task_name, task_mess)
+                trees.append(f'{task_name_for_next_step}>{task_name}')
+                task_name_for_next_step = task_name
+                xml_obj_pr = xml_obj
+                for xml_obj in xml_obj_pr :
+                    xml_obj_name = xml_obj.attrib["name"].lower()
+                    task_npp += 1
+                    task_name = f'T{task_npp}_{xml_obj_name}'
+                    task_mess = f'T{task_npp}: {xml_obj_name}'
+                    tasks[task_name] = cr_py_operator(task_npp, task_name, task_mess)
+                    trees.append(f'{task_name_for_next_step}>{task_name}')
+                    #task_name_for_next_step = task_name
+                    #xml_obj_pr = xml_obj
+
+    
+    for value_x in trees :
+        key_t = value_x[:value_x.find(">")]
+        value_t = value_x[value_x.find(">")+1:]
+        tasks[key_t].set_downstream(tasks[value_t])
+    
+
+
+            
+
+
 
         
 
